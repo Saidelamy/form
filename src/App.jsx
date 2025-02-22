6b56d36e89fcbae3931d5d8d6b2d98e95254882e
said magdy <saidmagdy511@gmail.com>
2025-02-22 16:51:14 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,46 @@
-import React, { useState } from "react";
+import React from "react";
+import { useForm } from "react-hook-form";
 
 function App() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const {
+    register,
+    reset,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
 
-  const [error, setError] = useState({ email: "", password: "" });
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    setEmail({ email: "", password: "" });
-
-    if (!email.includes("@")) {
-      setError({ ...error, email: "email must have @ sympol" });
-      return;
-    }
-    if (password.length < 8) {
-      setError({
-        ...error,
-        password: "password must be at least 8 charctars",
-      });
-      return;
-    }
-
-    console.log("submited");
+  function onSubmit() {
+    console.log("sasas");
+    // reset();
   }
+  console.log(watch("example")); // watch input value by passing the name of it
 
   return (
     <>
-      <form action="" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="ex: example@co.com"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        {error.email && <p>{error.email}</p>}
-        <input
-          type="password"
-          placeholder="ex: Ss@1122334455"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        {error.password && <p>{error.password}</p>}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <input
+            type="text"
+            placeholder="ex: example@co.com"
+            {...register("email", {
+              required: "email address is required",
+              maxLength: 20,
+            })}
+            defaultValue={"said"}
+          />
+          {errors.email && <p>{errors.email.message}</p>}
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="ex: Ss@1122334455"
+            {...register("password", { required: "password is required" })}
+          />
+          {errors.password && <p>{errors.password.message}</p>}
+        </div>
 
-        <button>submit</button>
+        <input type="submit" />
       </form>
     </>
   );
